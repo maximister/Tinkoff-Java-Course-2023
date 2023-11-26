f0c3f309a254581e640b07cf08dd3ea960fbb9d8
maximister <maximister44@gmail.com>
2023-11-27 00:34:36 +0300

    added serialization to speed up test 3

--- src/test/java/edu/hw7/task2/ParallelFactorialCounterTest.java
+++ src/test/java/edu/hw7/task2/ParallelFactorialCounterTest.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static edu.hw7.task2.ParallelFactorialCounter.getFactorial;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,10 +36,39 @@ public class ParallelFactorialCounterTest {
     @Test
     @DisplayName("testing parallel factorial with big value")
     public void parallelFactorialCounterTest() {
-        try (InputStream fis = new FileInputStream("src/test/java/edu/hw7/task2/longFactorial.txt")) {
-            String longString = IOUtils.toString(fis, StandardCharsets.UTF_8);
-            BigInteger expected = new BigInteger(longString.trim());
+        Path serializedFactPath = Path.of("src/test/java/edu/hw7/task2/saveFactorial.ser");
+        if (!Files.exists(serializedFactPath)) {
+            serializeFactorial100_000();
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream(serializedFactPath.toString());
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+
+            BigInteger expected = (BigInteger) objectInputStream.readObject();
+
             assertThat(getFactorial(100000)).isEqualTo(expected);
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void serializeFactorial100_000() {
+        //до этого записывал факториал 100_000 строкой в файл и считывал,
+        // но тк большой число преобразуется из строки вооде за n^2,
+        // тест был очень долгим, так что попробую поиграть с сериализацией
+        // и выполнить этот долгий процесс лишь раз
+
+        // P.S прирост в скорости в 5 раз, с учетом того,
+        // что встроеные средства вроде не самые быстрые, я доволен
+
+        try (FileOutputStream outputStream = new FileOutputStream("src/test/java/edu/hw7/task2/saveFactorial.ser");
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+             InputStream fis = new FileInputStream("src/test/java/edu/hw7/task2/longFactorial.txt")){
+
+                String longString = IOUtils.toString(fis, StandardCharsets.UTF_8);
+                BigInteger factorial = new BigInteger(longString.trim());
+                objectOutputStream.writeObject(factorial);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
