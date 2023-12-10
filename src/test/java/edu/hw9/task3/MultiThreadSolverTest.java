6f3ccf7bc942609c60c158e95fea1f4e7feeae33
maximister <maximister44@gmail.com>
2023-12-10 21:48:53 +0300

    task3  + tests

---
+++ src/test/java/edu/hw9/task3/MultiThreadSolverTest.java
@@ -0,0 +1,40 @@
+package edu.hw9.task3;
+
+import com.google.common.collect.Lists;
+import edu.hw9.task3.MultiThreadDfsSolver;
+import edu.project2.generator.DfsGenerator;
+import edu.project2.generator.Generator;
+import edu.project2.renderer.ConsoleRenderer;
+import edu.project2.renderer.Renderer;
+import edu.project2.solver.DfsSolver;
+import edu.project2.solver.Solver;
+import edu.project2.structures.Coordinate;
+import edu.project2.structures.Maze;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MultiThreadSolverTest {
+    @Test
+    @DisplayName("testing multi thread solver")
+    public void MultiThreadDfsSolverTest() {
+        Generator generator = new DfsGenerator();
+        Maze maze = generator.generate(51, 51);
+        Renderer renderer = new ConsoleRenderer();
+        Solver multiThreadSolver = new MultiThreadDfsSolver();
+        Solver oneThreadSolver = new DfsSolver();
+
+        List<Coordinate> oneThreadPath =
+            oneThreadSolver.solve(maze, new Coordinate(1, 1), new Coordinate(49, 49));
+
+
+        List<Coordinate> multiThreadPath =
+            multiThreadSolver.solve(maze, new Coordinate(1, 1), new Coordinate(49, 49));
+        //лабиринт идеальный => путь из начала в конец один =>
+        //тк однопоточный solver был уже протестирован и работает корректно,
+        // нам достаточно сравнить решение двух solver
+        assertThat(multiThreadPath).isEqualTo(Lists.reverse(oneThreadPath));
+    }
+}
